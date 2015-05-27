751de24a73e56d2ef2358e0e6b5db5807fc98b58
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-05-27 09:53:55 -0400

    Edit function to take block groups, tracts, or counties.

--- R/call_census_api.R
+++ R/call_census_api.R
@@ -6,7 +6,8 @@
 #' @param variables_to_get the variable name for the Census API call, 
 #' defined at \url{http://api.census.gov/}
 #' 
-#' @param geoids a vector of Census county FIPS codes
+#' @param geoids a vector of FIPS codes; must be at least to the county (5-digit)
+#' level, and can accept down to block groups (12-digit).
 #'
 #' @return a data_frame with each requested variable at each requested geography.
 #' 
@@ -18,22 +19,50 @@ call_census_api <- function(variables_to_get, geoids) {
   # parse geoid
   split_geo <- function(geoid) {
     list(st = substr(geoid,1,2), 
-         co = substr(geoid,3,5))
+         co = substr(geoid,3,5),
+         tr = substr(geoid,6,9),
+         bg = substr(geoid,10,12))
   } 
   
- 
   call_api_once <- function(variables_to_get, geoid) {
     newgeo <- split_geo(geoid)
-    st <- newgeo$st; co <- newgeo$co;
+    st <- newgeo$st; co <- newgeo$co; tr <- newgeo$tr; bg <- newgeo$bg;
     
-    url <- paste(
-      "http://api.census.gov/data/2013/acs5?get=",
-      paste(variables_to_get, collapse = ","),
-      "&for=county:", co,
-      "&in=state:", st,
-      "&key=1209214b319264ae3163b6d262dda4106e5c77f0",
-      sep = ""
-    )
+    # if using block groups
+    if(bg != ""){
+      url <- paste(
+        "http://api.census.gov/data/2013/acs5?get=",
+        paste(variables_to_get, collapse = ","),
+        "&for=block+group:", bg,
+        "&in=state:", st,
+        "+county:", co,
+        "+tract:", tr,
+        "&key=0eb99606e898c76c0fd80ab7f25b46ccb3bdb587",
+        sep = ""
+      )
+    # if using tracts
+    } else if(tr != ""){
+      url <- paste(
+        "http://api.census.gov/data/2013/acs5?get=",
+        paste(variables_to_get, collapse = ","),
+        "&for=tract:", tr,
+        "&in=state:", st,
+        "+county:", co,
+        "&key=0eb99606e898c76c0fd80ab7f25b46ccb3bdb587",
+        sep = ""
+      )
+    # if using counties
+    } else {
+      url <- paste(
+        "http://api.census.gov/data/2013/acs5?get=",
+        paste(variables_to_get, collapse = ","),
+        "&for=county:", co,
+        "&in=state:", st,
+        "&key=0eb99606e898c76c0fd80ab7f25b46ccb3bdb587",
+        sep = ""
+      )
+    }
+    
     
     # Gives back a list of lists; first list has the headers
     response <- httr::content(httr::GET(url))
