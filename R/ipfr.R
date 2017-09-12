b5103690d825acb415e0a01d2f44b33249ad0edd
Kyle Ward <kyle.ward@wsp.com>
2017-09-11 22:35:39 -0400

    Allow cluster specification on seed table
    If provided, expansion will respect the cluster assignment
    in the seed table. This allows household surveys to be
    expanded to sub-regional marginal controls.

--- R/ipfr.R
+++ R/ipfr.R
@@ -9,12 +9,13 @@ NULL
 
 #' Reweight a Seed Table to Marginal Controls
 #' 
-#' @param targets A \code{named list} of data frames.  Each name in the list defines a
-#'    marginal dimension and must match a column from the seed table.  The data
-#'    frame associated with each name must start with an identical \code{ID} column.
-#'    The other column names define the marginal categories that targets are
-#'    provided for.  Each row of the data frames represents a unique set of
-#'    targets to fit.  Every data frame must have the same number of rows.
+#' @param targets A \code{named list} of data frames.  Each name in the list 
+#'   defines a marginal dimension and must match a column from the seed table. 
+#'   The data frame associated with each name must start with an identical 
+#'   column named \code{cluster}. Each row in the target table defines a new 
+#'   cluster (these could be TAZs, tracts, districts, etc.), and every target 
+#'   table must have the same number of rows/clusters. The other column names
+#'   define the marginal categories that targets are provided for.
 #'    
 #' @param seed A \code{data frame} including a \code{weight} field and necessary
 #'    colums for matching to marginal targets.
@@ -65,31 +66,67 @@ ipf <- function(seed, targets,
   # throwing errors.
   for (name in names(targets)) {
     col_names <- colnames(targets[[name]])
-    col_names <- type.convert(col_names[!col_names == "ID"], as.is = TRUE)
+    col_names <- type.convert(col_names[!col_names == "cluster"], as.is = TRUE)
     
     test <- match(col_names, seed[[name]])
     if (any(is.na(test))) {
       prob_cat <- col_names[which(is.na(test))]
-      stop("Marginal ", name, "; category ", prob_cat, " is missing from seed table")
+      stop("Marginal ", name, "; category ", prob_cat[1], " is missing from seed table")
+    }
+  }
+  
+  # If the seed table includes a cluster column (assigning certain seed
+  # records to specific clusters), repeat the above test to make sure that
+  # each cluster has every observation.
+  if ("cluster" %in% colnames(seed)){
+    for (name in names(targets)) {
+      col_names <- colnames(targets[[name]])
+      col_names <- type.convert(col_names[!col_names == "cluster"], as.is = TRUE)
+      
+      for (cluster in seed$cluster){
+        test <- match(col_names, seed[[name]][seed$cluster == cluster])
+        if (any(is.na(test))) {
+          prob_cat <- col_names[which(is.na(test))]
+          stop("Marginal ", name, "; category ", prob_cat[1], " is missing from cluster ", cluster, " in the seed table.")
+        }   
+      }
     }
   }
   
   # Create df of totals from the first marginal table
   # (e.g. total households, persons, etc.)
   totals <- targets[[1]] %>%
-    tidyr::gather(key = category, value = count, -ID) %>%
-    dplyr::group_by(ID) %>%
+    tidyr::gather(key = category, value = count, -cluster) %>%
+    dplyr::group_by(cluster) %>%
     dplyr::summarize(total = sum(count))
   
   # Create a long data frame by repeating the seed table for each
-  # row in the target tables.
-  seed_long <- merge(totals$ID, seed) %>%
+  # row in the target tables. Call the first column ID to avoid potentially
+  # having two cluster columns in the table.
+  seed_long <- merge(totals$cluster, seed) %>%
     dplyr::rename(ID = x) %>%
     dplyr::arrange(ID)
   
-  # Convert the weights into percents.  The percents will sum to 1 for each ID
+  # If column 'cluster' is not present in the original seed table, it means
+  # that every seed record should be repeated for every cluster. If it is
+  # present, it means that specific seed records belong only to specific
+  # clusters (common in household expansion). If present, filter the data frame
+  # and remove then remove the cluster column.
+  if ("cluster" %in% colnames(seed)){
+    seed_long <- seed_long %>%
+      filter(ID == cluster) %>% 
+      select(-cluster)
+  }
+  
+  # Rename the ID field to cluster
   seed_long <- seed_long %>%
-    dplyr::group_by(ID) %>%
+    rename(cluster = ID)
+ 
+  
+  # Convert the weights into percents.  The percents will sum to 1 for each
+  # cluster
+  seed_long <- seed_long %>%
+    dplyr::group_by(cluster) %>%
     dplyr::mutate(weight = weight / sum(weight)) %>%
     dplyr::ungroup()
   
@@ -113,23 +150,23 @@ ipf <- function(seed, targets,
       
       # Prepare the target table
       target <- targets[[mName]] %>%
-        tidyr::gather(key = marg, value = target, -ID) %>%
+        tidyr::gather(key = marg, value = target, -cluster) %>%
         dplyr::mutate(marg = type.convert(marg, as.is = TRUE)) %>%
-        dplyr::group_by(ID) %>%
+        dplyr::group_by(cluster) %>%
         dplyr::mutate(target = target / sum(target)) %>%
         dplyr::ungroup() %>%
-        dplyr::arrange(ID)
-      colnames(target) <- c("ID", mName, "target")
+        dplyr::arrange(cluster)
+      colnames(target) <- c("cluster", mName, "target")
       
       # Prepare a summary of the seed table
       seed_summary <- seed_long %>%
-        dplyr::group_by_("ID", mName) %>%
+        dplyr::group_by_("cluster", mName) %>%
         dplyr::summarize(weight = sum(weight)) %>%
         dplyr::ungroup()
       
       # Join target to seed to calculate factor
       fac_tbl <- seed_summary %>%
-        dplyr::left_join(target, by = setNames(c("ID", mName), c("ID", mName))) %>%
+        dplyr::left_join(target, by = setNames(c("cluster", mName), c("cluster", mName))) %>%
         dplyr::mutate(factor = ifelse(weight == 0, 1, target / weight))
       
       # Handle missing targets.  For example, if the seed table had size
@@ -140,11 +177,11 @@ ipf <- function(seed, targets,
       
       # Prepare fac_tbl for joining to seed table
       fac_tbl <- fac_tbl %>%
-        dplyr::select(dplyr::one_of("ID", mName, "factor"))
+        dplyr::select(dplyr::one_of("cluster", mName, "factor"))
       
       # Join to the seed_long table and calculate new weight
       seed_long <- seed_long %>%
-        dplyr::left_join(fac_tbl, by = setNames(c("ID", mName), c("ID", mName))) %>%
+        dplyr::left_join(fac_tbl, by = setNames(c("cluster", mName), c("cluster", mName))) %>%
         dplyr::mutate(new_weight = weight * factor)
       
       # Because closure will depend on relative and absolute gap, add the
@@ -152,7 +189,7 @@ ipf <- function(seed, targets,
       # Remove rows from the table if the absolute diff is below the threshold.
       # This will keep them from keeping the IPF running.
       gap_tbl <- seed_long %>%
-        dplyr::left_join(totals, by = "ID") %>%
+        dplyr::left_join(totals, by = "cluster") %>%
         dplyr::mutate(
           old = total * weight,
           new = total * new_weight,
@@ -164,7 +201,7 @@ ipf <- function(seed, targets,
       # If every row in gap_tbl is below the absolute gap tolerance, then
       # collect the largest relative difference.  Otherwise, collect the
       # largest relative difference from the rows above the absolute gap
-      # tolerance.  Also, collect information on ID and category
+      # tolerance.  Also, collect information on cluster and category
       # to report out after IPF is complete.
       if (all(gap_tbl$abs_diff <= absolute_gap)) {
         rel_gap[i] <- max(gap_tbl$rel_diff)
@@ -176,7 +213,7 @@ ipf <- function(seed, targets,
       }
       pos <- which(gap_tbl$rel_diff == rel_gap[i])
       pos <- pos[1]
-      rel_id[i] <- gap_tbl$ID[pos]
+      rel_id[i] <- gap_tbl$cluster[pos]
       rel_cat[i] <- gap_tbl[[mName]][pos]
       abs_gap[i] <- gap_tbl$abs_diff[pos]
       v_converged[i] <- rel_gap[i] <= relative_gap | abs_gap[i] <= absolute_gap
@@ -197,7 +234,7 @@ ipf <- function(seed, targets,
   
   # After the loop, scale up the weights to match the totals
   seed_long <- seed_long %>%
-    dplyr::left_join(totals, by = "ID") %>%
+    dplyr::left_join(totals, by = "cluster") %>%
     dplyr::mutate(weight = weight * total) %>%
     dplyr::select(-total)
   
@@ -211,7 +248,7 @@ ipf <- function(seed, targets,
     position <- which(rel_gap == max(rel_gap))[1]
     message("Max Rel Gap:", rel_gap[position])
     message("Absolute Gap:", abs_gap[position])
-    message("ID:", rel_id[position])
+    message("cluster:", rel_id[position])
     message("Marginal:", names(targets)[position])
     message("Category:", rel_cat[position])
     utils::flush.console()
