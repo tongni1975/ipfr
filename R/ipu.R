6e853337540d859501ba3369cc149fee6266382c
Kyle Ward <kyle@caliper.com>
2019-08-04 22:47:42 -0400

    Move package documentation to separate R file

--- R/ipu.R
+++ R/ipu.R
@@ -1,17 +1,3 @@
-#' ipfr: A package to perform iterative proportional fitting
-#' 
-#' The main function is \code{\link{ipu}}. For a 2D/matrix problem, the 
-#' \code{\link{ipu_matrix}} function is easier to use. The resulting
-#' \code{weight_tbl} from \code{ipu()} can be fed into \code{\link{synthesize}}
-#' to generate a synthetic population
-#' 
-#' @docType package
-#' @importFrom utils type.convert
-#' @importFrom stats setNames
-#' @name ipfr
-NULL
-#> NULL
-
 #' Iterative Proportional Updating
 #' 
 #' @description A general case of iterative proportional fitting. It can satisfy
